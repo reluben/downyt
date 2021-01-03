9e9c6ebdba44f4061d19d1c2cd5ed69929ba8ae0
reluben <76892349+reluben@users.noreply.github.com>
2021-01-03 11:58:43 -0700

    Update simple_downyt.sh
    got rid of grep

--- simple_downyt.sh
+++ simple_downyt.sh
@@ -1,4 +1,5 @@
-wc -l list.txt ##how many lines there are in the text file (I get rid of all lines that not links in vim with this command :g!/^[hy]/d <-> go to every line that does not start with 'h' or 'y' and delete it)
+wc -l list.txt ##how many lines there are in the text file
+#I get rid of all lines that are not links in vim with this command :g!/^[hy]/d --> i.e. go to every line that does not start with 'h' or 'y' and delete it
 	youtube-dl --sub-lang en --write-auto-sub --write-sub --embed-sub -f '18/http-832/worst' --external-downloader aria2c --external-downloader-args '-c -j 3 -x 3 -s 3 -k 1M' -o "%(autonumber)s_%(id)s.%(ext)s" -a list.txt ##autonumbers them in case there is an order and adds their id - no title of video
 ls *.{mp4,webm,mkv} > play.m3u ##all the videos that are generated are added to a playlist file
 wc -l play.m3u ##displays how many videos were added to the playlist
