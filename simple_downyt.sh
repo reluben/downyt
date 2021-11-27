4b4b58cf366a18c6de1d9403457bbabb51352a68
reluben <76892349+reluben@users.noreply.github.com>
2021-11-27 11:29:03 -0700

    Update simple_downyt.sh

--- simple_downyt.sh
+++ simple_downyt.sh
@@ -1,19 +1,14 @@
-# how many lines there are in the text file
-wc -l list.txt 
-
-# I get rid of all lines that are not links in vim with this command
-# :g!/^[hy]/d
-# i.e. go to every line that does not start with 'h' or 'y' and delete it
-
-	yt-dlp --sub-lang en --write-auto-sub  --embed-sub -f '22+bestaudio/18+bestaudio/http-832/worst' -o "%(autonumber)s_$(titles)s_%(id)s.%(ext)s" -a list.txt 
-
-#autonumbers them in case there is an order and adds their id - no title of video
-
-#all the videos that are generated are added to a playlist file
-ls *.{mp4,webm,mkv} > play.m3u 
-
-#displays how many videos were added to the playlist
-wc -l play.m3u 
-
-#lets the user know the script ended
-echo "Done" 
+read -p "(1) Video or (2) Audio? " FORMAT
+if [ $FORMAT == 1 ]; then
+	wc -l list.txt # how many lines there are in the text file
+	yt-dlp --sub-lang en --write-auto-sub --embed-sub -f '22+bestaudio/http-832/worst' -o "%(autonumber)s_%(title)s_%(id)s.%(ext)s" -a list.txt
+	ls *.{mp4,mkv,webm} > play.m3u #all the videos that are generated are added to a playlist file
+	wc -l play.m3u #displays how many videos were added to the playlist
+	echo "~*~*~*~*Done" #lets the user know the script ended
+else
+	wc -l list.txt # how many lines there are in the text file
+	yt-dlp -f 'bestaudio' -x --audio-format mp3 -o "%(title)s_%(id)s.%(ext)s" -a list.txt
+	ls *.mp3 > music.m3u #all the songs that are generated are added to a playlist file
+	wc -l music.m3u #displays how many songs were added to the playlist
+	echo "~*~*~*~*Done" #lets the user know the script ended
+fi
